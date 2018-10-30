ff8a0d340bfde7113738330e0d1f884f92bc5d55
Victoria Collier <victoriacollier@Victorias-MacBook-Pro.local>
2018-10-29 22:22:04 -0400

    modified game

--- RPSGame.py
+++ RPSGame.py
@@ -5,15 +5,14 @@ from random import randint
 # arrays are 0-based -> the first item is 0, the second is 1, etc
 choices = ["Rock", "Paper", "Scissors"]
 player = False
+score1 = 5
+score2 = 5
 
 # make the computer choose a weapon from the choices array at random
 computer_choice = choices[randint(0, 2)]
 
-# show computers choice in the terminal window
-print("computer chooses: ", computer_choice)
-
 # set up our loop
-while player is False:
+while score1 > 0 and score2 > 0:
     # set player to True by making a selection
     print("Choose you weapon!")
     player = input("Rock, Paper, Scissors?\n")
@@ -28,26 +27,45 @@ while player is False:
         if computer_choice == "Paper":
             # computer won. crap!!
             print("You lose! Paper covers Rock")
+            score1 = score1 - 1
         else:
             # we win! such winning
             print("You win!", player, "smashes", computer_choice)
+            score2 = score2 - 1
 
     elif player == "Paper":
         if computer_choice == "Scissors":
             print("You lose!", computer_choice, "cut", player)
+            score1 = score1 - 1
         else:
             print("You win!", player, "covers", computer_choice)
+            score2 = score2 - 1
 
     elif player == "Scissors":
         if computer_choice == "Rock":
             print("You lose!", computer_choice, "smashes", player)
+            score1 = score1 - 1
         else:
             print("You win!", player, "cut", computer_choice)
+            score2 = score2 - 1
 
     elif player == "quit":
         exit()
     else:
         print("Check your spelling... that's not a valid choice\n")
+
+    # show computers choice in the terminal window
+    print("computer chooses: ", computer_choice)
+
+    # scoreboard
+    print('You', score1, 'Computer', score2)
+
+    # when lives run out
+    if score1 == 0:
+        print("Sorry you are out of lives. Better luck next time.")
+
+    elif score2 == 0:
+        print("Congrats you win!")
 
     # reset the game loop and start over again
     player = False
